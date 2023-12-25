36d4b45577ace1591f73789fb0412e7ef1942618
Nggohel <gohelnidhi15898@gmail.com>
2023-12-25 21:53:40 +0530

    add Counter code

--- components/CounterButton.js
+++ components/CounterButton.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
-function CounterButton() {
-     /*DO NOT EDIT THIS LINE */ console.log("CounterButton rendered!"); //DO NOT edit this line 
+function CounterButton({ onClick }) {
+  /*DO NOT EDIT THIS LINE */ console.log("CounterButton rendered!"); //DO NOT edit this line 
 
+  const handleClick = useCallback(() => {
+    onClick();
+  }, [onClick]);
+
+  return (
+    <button onClick={handleClick}>
+      Increment Counter
+    </button>
+  );
 }
 
-export default /*___________*/
+export default React.memo(CounterButton);
