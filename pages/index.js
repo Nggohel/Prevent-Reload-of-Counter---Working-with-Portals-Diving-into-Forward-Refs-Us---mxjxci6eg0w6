f981e6ff323284ae0d706a2e9c39792cf39ebf1d
Nggohel <gohelnidhi15898@gmail.com>
2023-12-25 21:58:58 +0530

    add Increment

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
-import { useState, useCallback } from 'react';
-import CounterButton from '../components/CounterButton';
+import { useState, useCallback } from "react";
+import CounterButton from "../components/CounterButton";
 
 export default function Home() {
-    const [count, setCount] = useState(0);
-    const [dummyState, setDummyState] = useState(0); // New state for testing
+  const [count, setCount] = useState(0);
+  const [dummyState, setDummyState] = useState(0); // New state for testing
 
-    const incrementCount = useCallback(() => {
-        setCount(prevCount => prevCount + 1);
-    }, []);
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
 
-    return (
-        <div>
-            <h1>Count: {count}</h1>
-            <CounterButton increment={incrementCount} /><br></br>
-            <button onClick={() => setDummyState(prev => prev + 1)}>Change Dummy State</button>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <CounterButton increment={increment} />
+      <br></br>
+      <button onClick={() => setDummyState((prev) => prev + 1)}>
+        Change Dummy State
+      </button>
+    </div>
+  );
 }
